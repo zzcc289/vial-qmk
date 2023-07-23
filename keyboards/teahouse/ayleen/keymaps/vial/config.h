d5c6dc417625ca6d1a25ccf9f9814da48991fa58
zheng.zhe <zzcc289@gmail.com>
2023-07-23 12:25:05 -0700

    Ayleen vial port

---
+++ keyboards/teahouse/ayleen/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA6, 0x9A, 0x90, 0xF5, 0x62, 0x47, 0xAC, 0x1E}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 8}
+#define VIAL_UNLOCK_COMBO_COLS {0, 5}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 4
