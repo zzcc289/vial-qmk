cc0139445b32e4608be087134280a732c8b845d4
zheng.zhe <zzcc289@gmail.com>
2023-07-23 18:44:40 -0700

    Forever65 vial port

---
+++ keyboards/forever65/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAE, 0xD6, 0xA6, 0xB7, 0x42, 0xE6, 0xB4, 0x77}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+
+#define VIAL_UNLOCK_COMBO_COLS {0, 12}
+
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 4
+#define VIAL_KEY_OVERRIDE_ENTRIES 4
