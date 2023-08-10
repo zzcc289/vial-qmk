dc5fe80f29a3901290533bfb62e407c5e40e00fa
zheng.zhe <zzcc289@gmail.com>
2023-08-09 21:00:38 -0700

    project alice vial port
    vial port, underglow control does not function

---
+++ keyboards/projectkb/alice/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB5, 0xB1, 0xB0, 0x2C, 0x1E, 0xB7, 0x03, 0x8A}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 15}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define VIAL_KEY_OVERRIDE_ENTRIES 8
