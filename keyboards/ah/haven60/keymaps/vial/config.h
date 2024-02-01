e62f460b39234e8f925fed5a76032c5ef3aa351b
zheng.zhe <zzcc289@gmail.com>
2024-02-01 14:39:28 +0800

    haven60 port

---
+++ keyboards/ah/haven60/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9D, 0xDA, 0x91, 0x48, 0xFC, 0xD7, 0xBA, 0x5D}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 13}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define DYNAMIC_KEYMAP_MACRO_COUNT 8
---
+++ keyboards/ah/haven60/keymaps/vial/rules.mk
@@ -0,0 +1,7 @@
+# This file intentionally left blank
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+
+QMK_SETTINGS = no
+KEY_OVERRIDE_ENABLE = no
