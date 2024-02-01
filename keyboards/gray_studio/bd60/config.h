b9aa069259065f8dfa8a37ff1378fe6f635b82b1
zheng.zhe <zzcc289@gmail.com>
2024-02-01 13:41:46 +0800

    bd60

---
+++ keyboards/gray_studio/bd60/config.h
@@ -0,0 +1,8 @@
+// Copyright 2023 Yizhen Liu (@edwardslau)
+// SPDX-License-Identifier: GPL-2.0
+#pragma once
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
---
+++ keyboards/gray_studio/bd60/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC9, 0xEF, 0x92, 0x5B, 0xD6, 0xC7, 0xE7, 0xC5} 
+
+#define VIAL_UNLOCK_COMBO_ROWS {0,2}
+
+#define VIAL_UNLOCK_COMBO_COLS {0,13}
