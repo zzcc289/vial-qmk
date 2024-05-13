ce542009042e6ec428c74ebd2d921b95e1aae663
zheng.zhe <zzcc289@gmail.com>
2024-05-13 22:27:42 +0800

    lint

---
+++ keyboards/melgeek/mj6xy/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x61, 0x0F, 0x99, 0xB8, 0x85, 0x78, 0xAB, 0x65}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 13}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define DYNAMIC_KEYMAP_MACRO_COUNT 8
